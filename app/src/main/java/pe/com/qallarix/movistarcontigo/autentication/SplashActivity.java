4f6b678bd73b73678b31834fa1d0af5a8c814405
ricardo morales <ricindigus@gmail.com>
2019-09-01 19:23:56 -0500

    agregado configuracion remota para forzar actualizacion

--- app/src/main/java/pe/com/qallarix/movistarcontigo/autentication/SplashActivity.java
+++ app/src/main/java/pe/com/qallarix/movistarcontigo/autentication/SplashActivity.java
@@ -4,13 +4,23 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,25 +43,65 @@ public class SplashActivity extends TranquiParentActivity {
     private String mDocumentType;
     private boolean conectado = true;
     private TextView tvVersionName;
-
+    private View vSplashSinConexion;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        bindViews();
+        setVersionName();
 
         if (internetConectionExists()){
-            setContentView(R.layout.activity_splash);
-            bindViews();
-            setVersionName();
-            if(existeToken())
-                validarSesion(mDocumentType,mDni,mToken);
-            else
-                mostrarLogin();
+            mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+            FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                    .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                    .setMinimumFetchIntervalInSeconds(0)
+                    .build();
+            mFirebaseRemoteConfig.setConfigSettings(configSettings);
+            HashMap<String,Object> defaults = new HashMap<>();
+            defaults.put("version_code",BuildConfig.VERSION_CODE);
+
+
+            mFirebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                @Override
+                public void onComplete(@NonNull Task<Boolean> task) {
+                    if (isOldVersion()){
+                        mostrarMensajeOldVersion();
+                    }else{
+                        if(existeToken())
+                            validarSesion(mDocumentType,mDni,mToken);
+                        else
+                            mostrarLogin();
+                    }
+                }
+            });
         }else{
             conectado = false;
-            setContentView(R.layout.sin_conexion_internet);
+            vSplashSinConexion.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void goToUpdateAppMovistarContigo() {
+        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
         }
     }
+
+    private boolean isOldVersion() {
+        int versionCode = (int) mFirebaseRemoteConfig.getLong("version_code");
+        if (BuildConfig.VERSION_CODE < versionCode){
+            return true;
+        }
+        return false;
+    }
+
+
 
     private void setVersionName() {
         tvVersionName.setText(BuildConfig.VERSION_NAME);
@@ -59,6 +109,7 @@ public class SplashActivity extends TranquiParentActivity {
 
     private void bindViews() {
         tvVersionName = findViewById(R.id.splash_tvVersionName);
+        vSplashSinConexion = findViewById(R.id.splash_vSinConexion);
     }
 
     @Override
@@ -168,6 +219,39 @@ public class SplashActivity extends TranquiParentActivity {
         }
     }
 
-
+    public void mostrarMensajeOldVersion(){
+        Toast mToast = Toast
+                .makeText(this, "Hay una nueva versión disponible de Movistar Contigo. " +
+                                "¡Actualiza y sigue disfrutando de nuestra app!",
+                Toast.LENGTH_SHORT);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this
+                ,R.style.DialogMensajeStyle);
+        builder.setTitle("NUEVA VERSIÓN DISPONIBLE");
+        builder.setMessage("Hay una nueva versión disponible de Movistar Contigo. " +
+                "¡Actualiza y sigue disfrutando de nuestra app!");
+        builder.setPositiveButton("ACTUALIZAR", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+                goToUpdateAppMovistarContigo();
+                finishAffinity();
+            }
+        });
+        builder.setNegativeButton("SALIR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finishAffinity();
+            }
+        });
+        builder.setCancelable(false);
+        final AlertDialog alertDialog = builder.create();
+        if(!isFinishing())
+        {
+            alertDialog.show();
+        }else{
+            mToast.show();
+            finishAffinity();
+        }
+    }
 
 }
